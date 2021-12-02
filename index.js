7bad810e1f35fcfa0cd2487438059669a04e9e00
lucky <takuto.yoshikai@gmail.com>
2021-12-02 21:40:23 +0900

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+const fs = require("fs");
+
+
+function dirsha(path) {
+  const text = fs.readdirSync(path).reduce((a, b) => {
+    return a + "/" + b;
+  });
+  console.log(text);
+  return crypto.createHash("sha256").update(text, "utf8").digest("hex");
+}
+
+function main() {
+  const path = process.argv[2];
+  const hash = dirsha(path);
+  console.log(hash);
+}
+
+if (require.main === module) {
+  main();
+} else {
+  module.exports = dirsha;
+}
